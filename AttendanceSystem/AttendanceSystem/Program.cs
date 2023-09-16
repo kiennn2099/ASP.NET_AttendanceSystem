14d257ea40f29aea493f5d163c32367ef4f687b3
Kien Nguyen <kiennn2099@gmail.com>
2023-09-16 09:03:52 +0700

    set appsetting and route

--- AttendanceSystem/AttendanceSystem/Program.cs
+++ AttendanceSystem/AttendanceSystem/Program.cs
@@ -20,6 +20,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    );
+
+//app.MapRazorPages();
 
 app.Run();
