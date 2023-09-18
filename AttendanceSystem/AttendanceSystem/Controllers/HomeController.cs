9f2bc2ccfebaad810d1509ddba376eaab2e3397b
Kien Nguyen <kiennn2099@gmail.com>
2023-09-19 01:37:30 +0700

    View Schedule

--- AttendanceSystem/AttendanceSystem/Controllers/HomeController.cs
+++ AttendanceSystem/AttendanceSystem/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AttendanceSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AttendanceSystem.Controllers
 {
@@ -8,5 +9,33 @@ namespace AttendanceSystem.Controllers
         {
             return View();
         }
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Schedule(IFormCollection f)
+        {
+            AttendanceSystemContext con = new AttendanceSystemContext();
+           
+            var query = from schedule in con.Schedules
+                        join subject in con.Subjects
+                        on schedule.SubjectId equals subject.SubjectId
+                        select new
+                        {
+                            schedule.Slot,
+                            schedule.DateTime,
+                            subject.SubjectName
+                        };
+
+            var result = query.ToList();
+
+            var sortedSchedules = result
+                .OrderBy(schedule => schedule.DateTime.Value.DayOfWeek)
+                .ToList();
+            ViewBag.Schedule = sortedSchedules;
+            return View();
+        }
+
     }
 }
